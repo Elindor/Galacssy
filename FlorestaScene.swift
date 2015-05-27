ff1233887cd3f87f2b22026d11d0216d68fa3ace
gabigomes94 <gah__@hotmail.com>
2015-05-27 14:14:03 -0300

    Retorno das telas (exceto Casa)

--- FlorestaScene.swift
+++ FlorestaScene.swift
@@ -12,6 +12,8 @@ import SpriteKit
 
 class FlorestaScene : SKScene {
     
+    let transition = SKTransition()
+    
     override func didMoveToView(view: SKView) {
         
         /* texto reponsável em exibir que o cenário da floresta esta em construção. */
@@ -26,6 +28,13 @@ class FlorestaScene : SKScene {
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent){
         /* Called when a touch begins */
+        
+        let touch = touches.first as! UITouch
+        let touchLocation = touch.locationInNode(self)
+        let cenarioMapa = GameScene(size: self.size)
+        cenarioMapa.scaleMode = SKSceneScaleMode.AspectFill
+        self.scene!.view?.presentScene(cenarioMapa, transition: transition)
+
     }
     
     override func update(currentTime: CFTimeInterval) {
